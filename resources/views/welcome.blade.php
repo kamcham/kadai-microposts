156b183c9d10a760c44803def61a45970eb77c32
kanna.satake <blqblqblq666@gmail.com>
2018-06-07 01:32:52 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,6 @@
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
         </div>
+         {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
     </div>
 @endsection
